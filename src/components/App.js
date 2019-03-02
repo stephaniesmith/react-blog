6ef6d5f8f3ac5f9cc35e4a403931436247d9bca6
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-01 19:50:30 -0800

    geting linter working

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = props => {
+  return (
+    <div className="ui container">
+      App!
+    </div>
+  );
+};
+
+App.propTypes = {
+
+};
+
+export default App;
