7f69c279015eff3cd205fba90a5d3965c9516606
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-02 11:41:03 -0800

    refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,9 +12,12 @@ export const fetchPosts = () => async dispatch => {
 
 export const fetchPostsAndUsers = () => async(dispatch, getState) => {
   await dispatch(fetchPosts());
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchUser = id => async dispatch => {
